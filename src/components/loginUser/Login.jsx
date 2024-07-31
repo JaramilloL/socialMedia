54b05d781f5c39afccdda319b07a79bd5adbc861
JLJL <juanluisjaramillolopez@gmail.com>
2024-07-31 17:57:27 -0600

    integracion de react-hook-form para la captura de los formularios tande de register como de login

--- src/components/loginUser/Login.jsx
+++ src/components/loginUser/Login.jsx
@@ -1,9 +1,34 @@
 //aqui pondremos la logica de la captura de datos
 
-const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+import { useForm } from "react-hook-form";
+import LoginFront from "./LoginFront";
 
-export default Login
+const Login = () => {
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+    getValues,
+  } = useForm(); //para atrpara los datos de formulario de forma autocontrolada
+
+  const onSubmit = handleSubmit((data) => {
+    try {
+      const email = getValues("email");
+      const password = getValues("password");
+      console.log(data);
+      console.log(email, password);
+      reset();
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+  //le pasamos las funciones de formular al componente
+  return (
+  <>
+    <LoginFront register={register} errors={errors} onSubmit={onSubmit} />
+  </>
+  );
+};
+
+export default Login;
--- src/components/registerUser/Register.jsx
+++ src/components/registerUser/Register.jsx
@@ -1,9 +1,34 @@
+import { useForm } from "react-hook-form";
+import RegisterFront from "./RegisterFront";
 //aqui tendremos toda la logica del registro hacia firebase
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+    getValues,
+  } = useForm(); //para atrpara los datos de formulario de forma autocontrolada
 
-export default Register
+  const onSubmit = handleSubmit((data) => {
+    try {
+      const userName = getValues("userName");
+      const email = getValues("email");
+      const password = getValues("password");
+      console.log(data);
+      console.log(userName, email, password);
+      reset();
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+
+  return (
+    <>
+      <RegisterFront onSubmit={onSubmit} register={register} errors={errors} />
+    </>
+  );
+};
+
+export default Register;
