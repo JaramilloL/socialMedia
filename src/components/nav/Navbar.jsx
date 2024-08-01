7e47d291a70fbb88024de42033db12590ac41cf3
JLJL <juanluisjaramillolopez@gmail.com>
2024-07-31 19:25:55 -0600

    agregacion del navbar responsivo

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,9 +1,69 @@
 //navbar para la navegacion de paginas
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+import {
+  AppBar,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { NavLink } from "react-router-dom";
+import NavList from "./NavList";
+import { useState } from "react";
 
-export default Navbar
+const Navbar = () => {
+  //manejo del estodo de clic para el navigation
+  const [open, setOpen] = useState(false)
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={() => setOpen(true)}
+            sx={{ display: { xs: "block", sm: "none" }, mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            SocialMedia
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <Button color="inherit">
+            <NavLink
+              to="/"
+              style={{ color: "inherit", textDecoration: "none" }}
+            >
+              Login
+            </NavLink>
+          </Button>
+          <Button color="inherit">
+            <NavLink
+              to="/register"
+              style={{ color: "inherit", textDecoration: "none" }}
+            >
+              Register
+            </NavLink>
+          </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        open={open}
+        anchor="left"
+        onClose={() => setOpen(false)}
+        sx={{ display: { xs: "block", sm: "none" } }}
+      >
+        <NavList onClick={() => setOpen(false)} />
+      </Drawer>
+    </Box>
+  );
+};
+
+export default Navbar;
--- src/layout/Index.jsx
+++ src/layout/Index.jsx
@@ -1,11 +1,12 @@
 //aqui se renderizaran los componentes de registro, login y navbar para el uaurio, ademas de cuando se autentique se manadara a la pagina de chat 
 
 import { Outlet } from "react-router-dom"
+import Navbar from "../components/nav/Navbar"
 
 const Index = () => {
   return (
     <div>
-        Index
+        <Navbar/>
         <Outlet/>
     </div>
   )
